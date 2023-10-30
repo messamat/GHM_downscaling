242bc08b56096ae96100a271ce09d12681473095
messamat <messamat@uw.edu>
2023-10-30 15:00:47 -0400

    Use mask to clipe pixel area and pixarea fraction rasters instead of just extents.

--- preprocessing/additional_hsgrids.py
+++ preprocessing/additional_hsgrids.py
@@ -8,12 +8,12 @@ arcpy.CheckOutExtension("Spatial")
 def get_continental_hsgrids(continentlist, upapath, pixarea_path, globallakes_fraction_15s, outputdir,
                             overwrite, verbose=True):
     arcpy.env.workspace = r'in_memory'
-    xmin, xmax, ymin, ymax = get_continental_extent(continentlist)
+    #xmin, xmax, ymin, ymax = get_continental_extent(continentlist)
 
     if verbose:
         print('Getting continental HydroSHEDS grids...')
 
-    #Identify and write out raster of HR cell pourpoint for each LR cell (but not guaranteed to perfectly align right?)
+    #Identify and write out raster of HR cell pourpoint for each LR cell
     cell_pourpoint_path = os.path.join(outputdir,
                                        '{}_cellpourpoint_15s.tif'.format(
                                            ''.join(continentlist)))
@@ -27,13 +27,15 @@ def get_continental_hsgrids(continentlist, upapath, pixarea_path, globallakes_fr
     #Clip HR pixel area and global lake fraction to the extent of the spatial union of continents in continentlist
     out_pixarea_clip = os.path.join(outputdir,'{}_pixarea_15s.tif'.format(''.join(continentlist)))
     if not arcpy.Exists(out_pixarea_clip) or overwrite:
-        arcpy.management.Clip(in_raster=pixarea_path,
-                              rectangle="{} {} {} {}".format(xmin, ymin, xmax, ymax),
-                              out_raster=out_pixarea_clip)
+        ExtractByMask(in_raster=pixarea_path,
+                      in_mask_data=cell_pourpoint_path,
+                      extraction_area='INSIDE').save(out_pixarea_clip)
 
     out_pixareafraction_glolakres_clip = os.path.join(outputdir,
-                 '{}_pixareafraction_glolakres_15s.tif'.format(''.join(continentlist)))
+                                                      '{}_pixareafraction_glolakres_15s.tif'.format(
+                                                          ''.join(continentlist)))
     if not arcpy.Exists(out_pixareafraction_glolakres_clip) or overwrite:
-        arcpy.management.Clip(in_raster=globallakes_fraction_15s,
-                              rectangle="{} {} {} {}".format(xmin, ymin, xmax, ymax),
-                              out_raster=out_pixareafraction_glolakres_clip)
+        ExtractByMask(in_raster=globallakes_fraction_15s,
+                      in_mask_data=cell_pourpoint_path,
+                      extraction_area='INSIDE').save(out_pixareafraction_glolakres_clip)
+
