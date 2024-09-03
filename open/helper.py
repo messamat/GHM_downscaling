718ca29c21d43d243ec9fc5d72eead01d0927461
Mathis MEssager <mathis.messager@mail.mcgill.ca>
2024-09-03 16:37:33 +0200

    Update Europe extent for WG3

--- open/helper.py
+++ open/helper.py
@@ -1,8 +1,10 @@
+#See ContinentInfo.csv from https://github.com/JKupzig/WaterGAPLite/blob/main/data-raw/ContinentInfo.csv for adding other continents
 def get_continental_extent(continentlist):
-    extentdict = {'eu': (-25, 70, 12, 84),
-                  'as': (57, 152, 0, 68),
-                  'si': (58, 180, 45, 84),
-                  'sa': (-93, -32, -56, 15)}
+    extentdict = {'eu': (-31.333333333, 52, 27.416666667, 80.833333333)}
+                  #,)
+                  #'as': (57, 152, 0, 68),
+                  #'si': (58, 180, 45, 84),
+                  #'sa': (-93, -32, -56, 15)}
     xmin, xmax, ymin, ymax = extentdict[continentlist[0]]
     if len(continentlist) > 1:
         for x in continentlist[1:]:
